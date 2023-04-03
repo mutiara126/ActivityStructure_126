5e1c4cabbefa181801a83afe3bfe6b832f74f87e
mutiara126 <salsamutiara1812@gmail.com>
2023-04-03 11:27:04 +0800

    menampilkan display mahasiswa2

--- ActivityStructure_126/ActivityStructure_126.cpp
+++ ActivityStructure_126/ActivityStructure_126.cpp
@@ -5,19 +5,31 @@ struct mahasiswa
 {
 	string NIM;
 	string nama;
-	string almaat;
+	string alamat;
 	int umur;
 };
 
 
 int main() {
-	mahasiswa mh1, mhs2;
+	mahasiswa mhs1, mhs2;
 
 	mhs1.NIM = "20210140019";
 	mhs1.nama = "Ardiyan Mulya";
 	mhs1.alamat = "Wonosobo";
 	mhs1.umur = 20;
 
+	cout << "Masukan NIM :";
+	cin >> mhs2.NIM;
+	cout << "Masukan Nama :";
+	cin >> mhs2.nama;
+	cout << "Masukan Alamat : ";
+	cin >> mhs2.alamat;
+	cout << "Masukan Umur : ";
+	cin >> mhs2.umur;
 
+	cout << "\nNIM = " << mhs1.NIM;
+	cout << "\nNama = " << mhs1.nama;
+	cout << "\nAlamat = " << mhs1.alamat;
+	cout << "\nUmur = " << mhs1.umur;
 
 }
